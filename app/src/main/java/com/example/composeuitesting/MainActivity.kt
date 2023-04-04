8f2c5a5a908431f07f2115a6d60a862f72757645
Fernando Esteban Dehesa Lozada <dehesafe@globalhitss.com>
2023-04-04 14:15:45 -0600

    Add viewBiending

--- app/src/main/java/com/example/composeuitesting/MainActivity.kt
+++ app/src/main/java/com/example/composeuitesting/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.composeuitesting
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
+import com.example.composeuitesting.databinding.ActivityMainBinding
 
 class MainActivity : FragmentActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/example/composeuitesting/compose/ComposeFragment.kt
@@ -0,0 +1,13 @@
+package com.example.composeuitesting.compose
+
+import androidx.fragment.app.Fragment
+
+class ComposeFragment: Fragment() {
+
+    companion object {
+        fun newInstance(): ComposeFragment {
+            return ComposeFragment()
+        }
+    }
+
+}
